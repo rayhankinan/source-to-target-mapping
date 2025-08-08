028de0d6726fa51719a5a81b8ec6c3f235f94786
rayhankinan <rayhankinan@gmail.com>
2025-08-09 00:02:01 +0700

    Feat: Add mime types checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   useCallback,
   useEffect,
@@ -9,11 +6,27 @@ import {
   useRef,
   useState,
 } from "react";
+import { match } from "ts-pattern";
+import { AlertCircleIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  CSV_MIME_TYPE,
+  XLS_MIME_TYPE,
+  XLSX_MIME_TYPE,
+} from "@/const/mime-types";
 
 function App() {
   const id = useId();
   const inputRef = useRef<HTMLInputElement>(null);
   const [file, setFile] = useState<File>();
+
+  const acceptedMimeTypes = useMemo(
+    () => [CSV_MIME_TYPE, XLS_MIME_TYPE, XLSX_MIME_TYPE].join(", "),
+    []
+  );
 
   const objectURL = useMemo(
     () => (file !== undefined ? URL.createObjectURL(file) : undefined),
@@ -50,9 +63,9 @@ function App() {
           id={id}
           ref={inputRef}
           onChange={onInputChange}
-          className="cursor-pointer disabled:cursor-not-allowed"
+          accept={acceptedMimeTypes}
           type="file"
-          accept="text/csv, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          className="cursor-pointer disabled:cursor-not-allowed"
         />
         <Button
           className="cursor-pointer disabled:cursor-not-allowed"
@@ -61,7 +74,27 @@ function App() {
           Clear File
         </Button>
       </div>
-      <p>Object URL: {objectURL}</p>
+      {file !== undefined && (
+        <>
+          {match(file !== undefined && file.type)
+            .with(XLS_MIME_TYPE, () => <p>XLS File Selected</p>)
+            .with(XLSX_MIME_TYPE, () => <p> XLSX File Selected</p>)
+            .with(CSV_MIME_TYPE, () => <p> CSV File Selected</p>)
+            .otherwise(() => (
+              <Alert
+                variant="destructive"
+                className="w-full max-w-md border-destructive"
+              >
+                <AlertCircleIcon />
+                <AlertTitle>Unsupported File Type</AlertTitle>
+                <AlertDescription>
+                  Please upload a file with one of the following MIME types:{" "}
+                  {acceptedMimeTypes}.
+                </AlertDescription>
+              </Alert>
+            ))}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/const/mime-types.ts
@@ -0,0 +1,4 @@
+export const CSV_MIME_TYPE = "text/csv";
+export const XLS_MIME_TYPE = "application/vnd.ms-excel";
+export const XLSX_MIME_TYPE =
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
