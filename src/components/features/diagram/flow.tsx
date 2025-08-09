9257fb4270ef51f7ddc517561b65ffdfdd5a3a21
rayhankinan <rayhankinan@gmail.com>
2025-08-10 03:00:45 +0700

    Feat: Table delete on node deletion

--- src/components/features/diagram/flow.tsx
+++ src/components/features/diagram/flow.tsx
@@ -1,14 +1,20 @@
-import { type JSX } from "react";
+import { useCallback, type JSX } from "react";
 import {
   Background,
   BackgroundVariant,
   Controls,
   MiniMap,
   ReactFlow,
+  type OnNodesDelete,
 } from "@xyflow/react";
+import { useMutation } from "@tanstack/react-query";
+import { useAsyncQueuer } from "@tanstack/react-pacer/async-queuer";
 import { useShallow } from "zustand/react/shallow";
 import useFlowStore from "@/stores/flow";
+import alasql from "alasql";
+import { toast } from "sonner";
 import AppPanel from "@/components/features/diagram/panel";
+import type { AppNode } from "@/types/flow";
 
 export default function AppFlow(): JSX.Element {
   const { nodes, edges, onNodesChange, onEdgesChange, onConnect } =
@@ -22,12 +28,40 @@ export default function AppFlow(): JSX.Element {
       }))
     );
 
+  const { mutateAsync: dropTableAsync } = useMutation({
+    mutationFn: async ({ label }: { label: string }) => {
+      await alasql.promise(`DROP TABLE IF EXISTS ${label}`);
+    },
+    onError: (_, { label }) => {
+      toast.error(`Failed to drop table ${label}`);
+    },
+  });
+
+  const { addItem } = useAsyncQueuer(
+    async ({ label }: { label: string }) => {
+      await dropTableAsync({ label });
+    },
+    {
+      concurrency: 1,
+    }
+  );
+
+  const onNodesDelete: OnNodesDelete<AppNode> = useCallback(
+    (nodes) => {
+      nodes.forEach((node) => {
+        addItem({ label: node.id });
+      });
+    },
+    [addItem]
+  );
+
   return (
     <div className="container mx-auto h-180 rounded-md border">
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
+        onNodesDelete={onNodesDelete}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         fitView
