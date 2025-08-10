a067c6f25878e542af4c2f736a33c63f3dab18b2
rayhankinan <rayhankinan@gmail.com>
2025-08-10 19:09:07 +0700

    Feat: Add download button

--- src/components/features/diagram/flow.tsx
+++ src/components/features/diagram/flow.tsx
@@ -13,6 +13,8 @@ import { useAsyncQueuer } from "@tanstack/react-pacer/async-queuer";
 import { useShallow } from "zustand/react/shallow";
 import useFlowStore from "@/stores/flow";
 import alasql from "alasql";
+import XLSX, { type WorkBook } from "xlsx";
+import { match } from "ts-pattern";
 import { toast } from "sonner";
 import { Download, Rows4 } from "lucide-react";
 import {
@@ -25,6 +27,7 @@ import {
 import AppPanel from "@/components/features/diagram/panel";
 import PreviewDialog from "@/components/features/diagram/preview-dialog";
 import type { AppNode } from "@/types/flow";
+import { MIME_TYPES } from "@/const/mime-types";
 
 export default function AppFlow(): JSX.Element {
   const [expandedNode, setExpandedNode] = useState<AppNode | null>(null);
@@ -40,6 +43,79 @@ export default function AppFlow(): JSX.Element {
         onConnect: state.onConnect,
       }))
     );
+
+  const { mutate: mutateDownload } = useMutation({
+    mutationFn: async ({ label, type }: { label: string; type: string }) =>
+      match(type)
+        .with(MIME_TYPES.CSV, async () => {
+          const rawString = await alasql.promise<string>(
+            `SELECT * INTO CSV({headers:true}) FROM ${label}`
+          );
+
+          const blob = new Blob([rawString], {
+            type: MIME_TYPES.CSV,
+          });
+
+          return blob;
+        })
+        .with(MIME_TYPES.XLS, async () => {
+          const wb = await alasql.promise<WorkBook>(
+            `SELECT * INTO XLSX({headers:true}) FROM ${label}`
+          );
+
+          const u8Array: ArrayBuffer = XLSX.write(wb, {
+            bookType: "xls",
+            type: "array",
+          });
+
+          const blob = new Blob([u8Array], {
+            type: MIME_TYPES.XLS,
+          });
+
+          return blob;
+        })
+        .with(MIME_TYPES.XLSX, async () => {
+          const wb = await alasql.promise<WorkBook>(
+            `SELECT * INTO XLSX({headers:true}) FROM ${label}`
+          );
+
+          const u8Array: ArrayBuffer = XLSX.write(wb, {
+            bookType: "xlsx",
+            type: "array",
+          });
+
+          const blob = new Blob([u8Array], {
+            type: MIME_TYPES.XLSX,
+          });
+
+          return blob;
+        })
+        .otherwise(async () => alasql.promise<Blob>(`SELECT * FROM ${label}`)),
+    onSuccess: (data, { label }) => {
+      const extension = match(data.type)
+        .with(MIME_TYPES.CSV, () => "csv")
+        .with(MIME_TYPES.XLS, () => "xls")
+        .with(MIME_TYPES.XLSX, () => "xlsx")
+        .otherwise(() => "txt");
+
+      const url = URL.createObjectURL(data);
+
+      try {
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = `${label}.${extension}`;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      } finally {
+        URL.revokeObjectURL(url);
+      }
+    },
+    onError: (error, { label }) => {
+      console.error(error);
+      toast.error(`Failed to download the file ${label}.`);
+    },
+  });
 
   const { mutateAsync: dropTableAsync } = useMutation({
     mutationFn: async ({ label }: { label: string }) => {
@@ -113,7 +189,15 @@ export default function AppFlow(): JSX.Element {
                   <Rows4 />
                 </ContextMenuShortcut>
               </ContextMenuItem>
-              <ContextMenuItem className="cursor-pointer disabled:cursor-not-allowed">
+              <ContextMenuItem
+                className="cursor-pointer disabled:cursor-not-allowed"
+                onClick={() =>
+                  mutateDownload({
+                    label: expandedNode.data.label,
+                    type: expandedNode.data.file.type,
+                  })
+                }
+              >
                 Download
                 <ContextMenuShortcut>
                   <Download />
